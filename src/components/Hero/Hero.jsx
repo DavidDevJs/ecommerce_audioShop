02e861ef9c7693c5b99d116b8a967dea56cdc406
David Gonzalez <davidgonzalez0519@gmail.com>
2021-05-05 18:15:23 -0500

    [ADD] Hero refactor creational component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Container, ButtonSEE } from './HeroStyle';
+
+export const Hero = () => {
+  return (
+    <>
+      <Container>
+        <p>New Product</p>
+        <h1>
+          XX99 MARK II <br /> HEADPHONE
+        </h1>
+        <p>
+          Experience natural, lifelike audio and exepcional build quality made
+          for the passionate music enthusiast.
+        </p>
+        <ButtonSEE>SEE PRODUCT</ButtonSEE>
+      </Container>
+    </>
+  );
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import { Hero, ButtonSEE } from './HomeStyled.js';
+import { Hero } from '../../components/Hero/Hero';
 
 export const Home = () => {
   return (
     <>
-      <Hero>
-        <p>New Product</p>
-        <h1>
-          XX99 MARK II <br /> HEADPHONE
-        </h1>
-        <p>
-          Experience natural, lifelike audio and exepcional build quality made
-          for the passionate music enthusiast.
-        </p>
-        <ButtonSEE>SEE PRODUCT</ButtonSEE>
-      </Hero>
+      <Hero />
     </>
   );
 };
